fde3f320460bfd3ca09400f0574e4a4ffe11bb84
Donald Nwokoro <infect3dlab@gmail.com>
2023-06-16 13:14:28 +0100

    Welcome to JavaScript!

---
+++ script.js
@@ -0,0 +1,14 @@
+// Values and Variables
+console.log("Hello World!");
+
+
+let firstName = "Donald";
+
+console.log(firstName);
+
+
+// Variable Naming Conventions
+let last_name2 = "John"
+let $function = 50;
+
+console.log(last_name2);
