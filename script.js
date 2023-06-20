14d3f7f661907aae261874a3e493e0cbe99b0779
Donald Nwokoro <infect3dlab@gmail.com>
2023-06-20 13:18:17 +0100

    Introduction to Data Types

--- script.js
+++ script.js
@@ -11,4 +11,26 @@ console.log(firstName);
 let last_name2 = "John"
 let $function = 50;
 
+let birthYear = 1990;
+
 console.log(last_name2);
+
+
+// Data Types
+
+// 1. Booleans->True or False Values
+var javaScriptIsEasy = true;
+var lifeisHard = false;
+
+console.log(typeof lifeisHard);
+
+// 2. Numbers
+donaldAge = 150;
+nonsoAge = 90.5;
+
+console.log(typeof donaldAge);
+console.log(typeof nonsoAge);
+
+// 3. Strings
+let message = "Welcome to Dubai";
+console.log(typeof message);
