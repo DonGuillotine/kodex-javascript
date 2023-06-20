258bfdb9f01b726163ab45a64cab82be3a5ff462
Donald Nwokoro <infect3dlab@gmail.com>
2023-06-20 13:51:01 +0100

    Datatypes: String

--- script.js
+++ script.js
@@ -34,3 +34,31 @@ console.log(typeof nonsoAge);
 // 3. Strings
 let message = "Welcome to Dubai";
 console.log(typeof message);
+
+let divine = '24';
+console.log(typeof divine)
+
+
+let henry = '24' + 2;
+console.log(typeof henry);
+
+
+//4. undefined
+let year;
+console.log(year);
+console.log(typeof year)
+
+
+// An empty value has nothing to do with undefined.
+let david = '';
+console.log(typeof david);
+
+
+// Value is undefined, type is undefined
+let car = undefined;
+console.log(typeof car)
+
+
+// When adding a number and a string, JavaScript will treat the number as a string.
+let vehicle = 16 + 4 + "Volvo"
+console.log(vehicle);
