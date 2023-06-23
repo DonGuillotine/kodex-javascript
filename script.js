eba305095172bb62ed51824af546fbb3e9b91e02
Donald Nwokoro <infect3dlab@gmail.com>
2023-06-23 11:24:08 +0100

    Coding Challange correction

--- script.js
+++ script.js
@@ -272,3 +272,23 @@ TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1
 TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
 
 */
+
+// Step 1 - Store Mark and John's mass and height in variables
+let markMass, markHeight, johnMass, johnHeight, markBMI, johnBMI, markHigherBMI;
+markMass = 78;
+markHeight = 1.69;
+
+johnMass = 92;
+johnHeight = 1.95;
+
+
+// Step 2 - Mark and John's BMI using the formular: BMI = mass / height ** 2 
+markBMI = markMass / markHeight ** 2; // markMass / markHeight * markHeight
+johnBMI = johnMass / johnHeight ** 2;
+
+console.log(markBMI, johnBMI);
+
+
+// Step 3 - Create a boolean variable 'markHigherBMI'
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
