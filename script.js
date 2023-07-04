8d95e59bc0a9d3fc816dcd6dac4d74499797abdf
Donald Nwokoro <infect3dlab@gmail.com>
2023-07-04 14:17:01 +0100

    The Ternary Operator Coding Challange Solution

--- script.js
+++ script.js
@@ -579,3 +579,7 @@ HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
 
 GOOD LUCK 😀
 */
+
+const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total valuse is ${bill + tip}`);
