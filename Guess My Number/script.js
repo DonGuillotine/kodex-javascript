f42cbc4948174f851f79541e464fc22a6cf3138d
Donald Nwokoro <infect3dlab@gmail.com>
2023-07-19 12:23:49 +0100

    Guess My Number!: Starting out logic

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// Secret Number Created!
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+
+const displayMessage = function(msg){
+    document.querySelector('.message').textContent = msg;
+};
+
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if(!guess){
+        displayMessage('No Number 💀');
+    }
+});
